506d7fbd6b281ceea92f52cf46b17b64addf6077
michaelrhodes <spam@michaelrhod.es>
2019-03-12 13:51:56 +1000

    Improve module variable naming

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 module.exports = dexy
 
 function dexy () {
-  var current = false
+  var running = false
   var subtests = 0
   var queue = []
 
   return test
 
   function test (fn) {
-    current ?
+    running ?
       (queue.splice(subtests++, 0, fn)) :
       (queue.push(fn), run())
   }
 
   function run () {
     var test = queue.shift()
-    current = !!test
+    running = !!test
     subtests = 0
 
-    current && test(function () {
+    running && test(function () {
       setTimeout(function () {
-        run(current = false)
+        run(running = false)
       })
     })
   }
